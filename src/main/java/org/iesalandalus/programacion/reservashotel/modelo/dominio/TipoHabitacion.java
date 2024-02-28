4ba53c21860a252403340f22e4a78d23b8893ec9
You-loop-great <silvia.cg12@gmail.com>
2024-02-28 20:46:38 +0100

    Cambios en enum TipoHabitacion

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
@@ -2,36 +2,29 @@ package org.iesalandalus.programacion.reservashotel.modelo.dominio;
 
 public enum TipoHabitacion {
 
-    SUITE("Suite",4),
-    SIMPLE("Simple",1),
-    DOBLE("Doble",2),
-    TRIPLE("Triple",3);
+    //Elimino el parámetro del nº max. de personas
 
-    /**2-Creo el atributo privado de tipo String cadenaAMostrar. */
+    SUITE("Suite"),
+    SIMPLE("Simple"),
+    DOBLE("Doble"),
+    TRIPLE("Triple");
 
-    private String cadenaAMostrar; //ambos atributos son de visibilidad privada
-    private int numeroMaximoPersonas;
 
-    /**3-Creo el constructor privado con el mismo nombre que la clase le paso por parámetros los atributos declarados.*/
+    private String cadenaAMostrar;
 
-    private TipoHabitacion(String cadenaAMostrar, int numeroMaximoPersonas){
+    // Elimino numero máximo de personas
+
+
+    TipoHabitacion(String cadenaAMostrar){ //Modifico el constructor del enumerado para no incluir el nº max. de personas
 
         this.cadenaAMostrar=cadenaAMostrar;
-        this.numeroMaximoPersonas=numeroMaximoPersonas;
     }
 
-    /**4- Creo el método público getter que devuelve el número máximo de personas que pueden alojarse en según qué tipo de habitación*/
-    public int getNumeroMaximoPersonas() {
-
-        return numeroMaximoPersonas;
-    }
-
-    /**5-Creo el método toString que devuelve la cadena que esperan los tests.*/
+    // Elimino el método getNumeroMaximoDePersonas
 
     @Override
     public String toString() {
 
-        //return "La habitación de tipo " + cadenaAMostrar + " tiene una capacidad de " + numeroMaximoPersonas + " personas.";
         return ordinal() + " .- " + cadenaAMostrar;
     }
 
